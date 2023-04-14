ae0d1ae4955bb674def11d1974530aac26ee9042
Dawid <daw.parc@gmail.com>
2023-04-14 17:21:21 +0200

    Upgrade contact page

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+
+const upload = multer({
+  dest: 'uploads/',
+});
 
 const app = express();
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -30,6 +36,18 @@ app.get('/info', (req, res) => {
 
 app.get('/history', (req, res) => {
   res.render('history');
+});
+
+app.post('/contact/send-message', upload.single('uploaded_file'), (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+  
+  if(author && sender && title && message && req.file ) {
+    res.render('contact', { isSent: true, uploaded_file: req.file.originalname });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.use((req, res) => {
